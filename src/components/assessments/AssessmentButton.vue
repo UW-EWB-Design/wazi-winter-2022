b9287c2cf5cc9f293b8b05883f97a3bcbd4ebbd5
AnaPaulaRamon <apramong@uwaterloo.ca>
2021-11-09 00:19:10 -0500

    added assessment button and list of cards

---
+++ src/components/assessments/AssessmentButton.vue
@@ -0,0 +1,40 @@
+<template>
+<button class="AssessmentButton">
+  <span class="text-lg leading-tight">+</span><slot>Take an Assessment</slot>
+</button>
+</template>
+
+<script>
+
+export default {
+  name: "AssessmentButton",
+}
+</script>
+
+<style scoped>
+.AssessmentButton {
+  background: white;
+  border: orange 2px solid;
+  color: orange;
+  cursor: pointer;
+  font-size: 0.75rem;
+  padding: 10px 25px;
+  transition-duration: 0.4s;
+  font-weight: 700;
+  font-size: 15px;
+  width: 640px;
+}
+
+.AssessmentButton:hover {
+  background-color: orange;
+  color: black;
+}
+
+span {
+  float: left;
+  left: -14px;
+  position: relative;
+}
+
+
+</style>
