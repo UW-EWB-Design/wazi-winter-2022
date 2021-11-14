b79300dd09003d5b285ca0a16096464de913af17
michelleshx <michelleshx462@gmail.com>
2021-11-14 14:54:41 -0500

    update dropdown selection

--- src/components/AssessmentsPage/Toolbar.vue
+++ src/components/AssessmentsPage/Toolbar.vue
@@ -1,19 +1,19 @@
 <template>
 	<div class="w-full flex justify-center">
 		<!-- Advanced Filter button: Opens modal -->
-		<button class="relative font-bold rounded-sm w-24 py-2 my-2 border border-black duration-300 hover:bg-gray-100 cursor-pointer">
+		<button class="relative font-bold rounded-sm w-24 py-2 my-2 h-10 border border-black duration-300 hover:bg-gray-100">
 			<span class="absolute left-3 text-lg leading-tight">+</span>Filter
 		</button>
 		<!-- Filter Dropdown -->
-		<div class="relative inline-block">
+		<div class="relative w-max">
 			<!-- Button -->
 			<button
-				class="flex justify-center rounded-sm border px-4 py-2 mt-2 mx-5 w-64 border-black duration-300 hover:bg-gray-100"
+				class="z-50 flex rounded-sm h-10 px-4 py-2 mt-2 mx-5 border border-black duration-300 hover:bg-gray-100"
 				aria-controls="filter-menu"
 				aria-expanded="false"
 				@click="isOpen = !isOpen"
 			>
-				<p>View <b>All Assessments</b></p>
+				<p>View <b>{{assessmentFilter}} Assessments</b></p>
 				<svg class="h-5 w-5 ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
 					<path
 						fill-rule="evenodd"
@@ -33,15 +33,15 @@
 			>
 				<div
 					v-show="isOpen"
-					class="z-10 mx-5 absolute w-64 rounded-sm focus:outline-none cursor-pointer text-sm font-bold"
+					class="px-5 absolute w-full rounded-sm cursor-pointer text-sm font-bold bg-white "
 					role="menu"
 					aria-orientation="vertical"
 					aria-labelledby="filter-menu"
 				>
-                    <a class="block px-4 py-2  hover:bg-gray-100 border border-black border-t-0" role="menuitem" @click="filter('All')">All</a>
-					<a class="block px-4 py-2 hover:bg-gray-100 border border-black border-t-0" role="menuitem" @click="filter('option1')">High</a>
-					<a class="block px-4 py-2 hover:bg-gray-100 border border-black border-t-0" role="menuitem" @click="filter('option2')">Moderate</a>
-					<a class="block px-4 py-2 hover:bg-gray-100 border border-black border-t-0" role="menuitem" @click="filter('option3')">Low</a>
+					<a class="block px-4 py-2 hover:bg-gray-100 border border-black border-t-0 duration-300" role="menuitem" @click="filter('all')">All</a>
+					<a class="block px-4 py-2 hover:bg-gray-100 border border-black border-t-0 duration-300" role="menuitem" @click="filter('high')">High</a>
+					<a class="block px-4 py-2 hover:bg-gray-100 border border-black border-t-0 duration-300" role="menuitem" @click="filter('moderate')">Moderate</a>
+					<a class="block px-4 py-2 hover:bg-gray-100 border border-black border-t-0 duration-300" role="menuitem" @click="filter('low')">Low</a>
 				</div>
 			</transition>
 		</div>
@@ -51,20 +51,30 @@
 <script>
 export default {
 	data: () => ({
+        assessmentFilter:'All',
 		isOpen: false,
 	}),
-    methods: {
-        filter(option) {
-            // Filter array
-            if (option === 'option1') {
-            } else if (option === 'option2') {
-
-            } else if (option === 'option3') {
-
-            } else {
-
-            }
-        }
-    }
+	methods: {
+		filter(option) {
+            this.isOpen = false;
+			// Pass selected filter to parent component
+			if (option === "high") {
+                // Sets local state
+                this.assessmentFilter = "High";
+                // Passes state to parent
+				this.$emit("filter", "high");
+				
+			} else if (option === "moderate") {
+                this.assessmentFilter = "Moderate";
+				this.$emit("filter", "moderate");
+			} else if (option === "low") {
+                this.assessmentFilter = "Low";
+				this.$emit("filter", "low");
+			} else {
+                this.assessmentFilter = "All";
+				this.$emit("filter", "all");
+			}
+		},
+	},
 };
 </script>
--- src/views/AssessmentHistoryPage.vue
+++ src/views/AssessmentHistoryPage.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="assessment-history">
-		<Toolbar />
+		<Toolbar v-on:filter="filter" />
+        <!-- Here for testing (remove after): -->
+        <p >{{assessmentFilter}}</p>
 	</div>
 </template>
 
@@ -8,8 +10,19 @@
 import Toolbar from "../components/AssessmentsPage/Toolbar.vue";
 export default {
 	name: "AssessmentHistory",
+    data() {
+        return {
+            // Pass to card list to use for filtering
+            assessmentFilter: 'all',
+        };
+    },
 	components: {
 		Toolbar,
 	},
+    methods: {
+        filter(option) {
+            this.assessmentFilter = option;
+        }
+    }
 };
 </script>
