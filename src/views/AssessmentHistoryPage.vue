6732fd9990b7f557662adfd153fa3f2983d0d338
Nadia <bn3nguye@uwaterloo.ca>
2021-11-28 14:58:45 -0500

    list of assessment cards

--- src/views/AssessmentHistoryPage.vue
+++ src/views/AssessmentHistoryPage.vue
@@ -1,14 +1,25 @@
 <template>
-	<div class="assessment-history">
+	<div class="assessment-history ">
 		<Toolbar v-on:filter="filter" v-on:advancedFilter="advancedFilter" />
 		<!-- Here for testing (remove after): -->
 		<p>Simple filtering selection: {{ assessmentFilter }}</p>
 		<p>Advanced filtering selection: {{ advancedFilters }}</p>
+
+		<div class="max-w-md mx-auto">
+			<ul>
+				<!-- List of assessments -->
+				<li v-for="assessment in assessments" :key="assessment.id">
+					<AssessmentCard :assessment="assessment" />
+				</li>
+			</ul>
+		</div>	
+
 	</div>
 </template>
 
 <script>
 import Toolbar from "../components/AssessmentsPage/Toolbar.vue";
+import AssessmentCard from "../components/HomePage/assessments/AssessmentCard.vue";
 export default {
 	name: "AssessmentHistory",
 	data() {
@@ -16,10 +27,48 @@ export default {
 			// Pass to card list to use for filtering
 			assessmentFilter: "All",
 			advancedFilters: "Advanced",
+			assessments: [
+				{
+					id: 1,
+					name: "Coronavirus Anxiety Scale (CAS)",
+					description: "Measures coronavirus-related anxiety",
+					level: "High",
+					datetime: "2021-11-12T05:02:29+0000",
+				},
+				{
+					id: 2,
+					name: "Coronavirus Anxiety Scale (CAS) This assessment has a very long name",
+					description: "Measures coronavirus-related anxiety. This assessment has a very long description",
+					level: "Low",
+					datetime: "2021-11-12T05:02:29+0000",
+				},
+				{
+					id: 3,
+					name: "Coronavirus Anxiety Scale (CAS)",
+					description: "Measures coronavirus-related anxiety",
+					level: "Moderate",
+					datetime: "2021-11-12T05:02:29+0000",
+				},
+				{
+					id: 4,
+					name: "Coronavirus Anxiety Scale (CAS)",
+					description: "Measures coronavirus-related anxiety",
+					level: "High",
+					datetime: "2021-11-12T05:02:29+0000",
+				},
+				{
+					id: 5,
+					name: "Coronavirus Anxiety Scale (CAS)",
+					description: "Measures coronavirus-related anxiety",
+					level: "Moderate",
+					datetime: "2021-11-12T05:02:29+0000",
+				},
+			],
 		};
 	},
 	components: {
 		Toolbar,
+		AssessmentCard,
 	},
 	methods: {
 		filter(option) {
